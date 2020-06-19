d9d9c678db4e59e46a98b95b8c3843876bddb6f4
abenzlaouia <mmelek1001@gmail.com>
2020-06-19 11:51:40 +0100

    add comment

--- models/player.js
+++ models/player.js
@@ -1,11 +1,10 @@
-const {model,Schema}=require('mongoose')
-const playerSchema=new Schema({
-_id:Schema.Types.ObjectId,
-firstName:String,
-lastName:String,
-post:String,
-profile:{type:Schema.Types.ObjectId,ref:'User'}
-
-})
-
-module.exports = model('Player',playerSchema)
+const { model, Schema } = require("mongoose");
+const playerSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  firstName: String,
+  lastName: String,
+  post: String,
+  profile: { type: Schema.Types.ObjectId, ref: "User" },
+});
+//branch dev change
+module.exports = model("Player", playerSchema);
